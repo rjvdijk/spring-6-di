2a8fccd34e7da346e02efc4224962e53152d509f
Rob van Dijk <rob@optimust.nl>
2024-01-29 16:04:19 +0100

    DI with Spring

--- src/test/java/guru/springframework/spring6di/controllers/ConstuctorInjectedControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/ConstuctorInjectedControllerTest.java
@@ -3,17 +3,16 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ConstuctorInjectedControllerTest {
 
+    @Autowired
     ConstuctorInjectedController constuctorInjectedController;
-
-    @BeforeEach
-    void setUp() {
-        constuctorInjectedController = new ConstuctorInjectedController(new GreetingServiceImpl());
-    }
 
     @Test
     void testSayHello() {
