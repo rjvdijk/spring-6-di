185db095448de88ae7b53fbdfc9594ce5f35f1fa
Rob van Dijk <rob@optimust.nl>
2024-01-29 16:27:30 +0100

    Using Qualifiers

--- src/main/java/guru/springframework/spring6di/controllers/ConstuctorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstuctorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,7 +9,7 @@ public class ConstuctorInjectedController {
 
     private final GreetingService greetingService;
 
-    public ConstuctorInjectedController(GreetingService greetingService) {
+    public ConstuctorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
